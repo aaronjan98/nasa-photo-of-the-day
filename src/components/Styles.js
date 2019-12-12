4d4c25c5d898d40c3b8ef7e06887bd0722706710
aaronjan <aaronjan98@gmail.com>
2019-12-12 13:28:14 -0800

    imported react card into ApodCard.js

---
+++ src/components/Styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const MainImg = styled.img`
+    border-radius: 10px;
+    box-shadow: -5px 3px 15px red;
+`;
+
+export const AstroDiv = styled.div`
+    margin 10px;
+`;
--- src/components/apodCard.js
+++ src/components/apodCard.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { MainImg, AstroDiv } from "./Styles.js";
+import {
+    CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
 
 const ApodCard = props => {
     
     return (
-        <div className="astronomyInfo" key={props.service_version}>
-            <img src={props.url} alt={'NASAs APOD'}/>
-            <h2>{props.title}</h2>
-            <h4>{props.date}</h4>
-            <p>{props.explanation}</p>
-        </div>
+        <AstroDiv>
+            <MainImg top width="100%" src={props.url} alt={'NASAs APOD'} key={props.service_version}/>
+            <CardBody>
+                <CardTitle>{props.title}</CardTitle>
+                <CardSubtitle>{props.date}</CardSubtitle>
+                <CardText>{props.explanation}</CardText>
+                <Button>Previous Day</Button>
+                <Button>Next Day</Button>
+            </CardBody>
+        </AstroDiv>
     );
 };
 
